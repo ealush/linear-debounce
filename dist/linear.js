a35b1c8b06a0a39d62f6b886835c39991ffef72a
Evyatar <evyatar.a@fiverr.com>
2019-02-21 19:28:59 +0200

    Build

--- dist/linear.js
+++ dist/linear.js
@@ -7,7 +7,7 @@
 		exports["linear"] = factory();
 	else
 		root["linear"] = factory();
-})(this, function() {
+})(typeof self !== 'undefined' ? self : this, function() {
 return /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
